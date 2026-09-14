1f65c20d4e7266ba7162ab00a163caa0c4d2da17
ARKLightning06 <64155125+ARKLightning06@users.noreply.github.com>
2026-09-14 16:43:27 -0400

    implement reduce method

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -2,6 +2,23 @@ package lab1;
 
 public class Reduce {
     public static void main(String[] args) {
-        
+        System.out.printf("Reduce: " + reduce(100));
+    }
+
+    static int reduce(int n) {
+        // returns the number of steps to reduce an int n to zero by following this pattern:
+        // if even, divide by 2
+        // if odd, subtract 1
+        // precondition: n >= 0
+        if (n == 0) {
+            return 0;
+        }
+        else if (n % 2 == 0) {
+            return 1 + reduce(n / 2);
+        }
+        else {
+            return 1 + reduce(n - 1);
+        }
     }
 }
+

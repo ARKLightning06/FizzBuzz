6ae0253966017bf2e150ace3d0948c478b26daaf
ARKLightning06 <64155125+ARKLightning06@users.noreply.github.com>
2026-09-14 16:00:02 -0400

    refactor fizzbuzz program

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -6,31 +6,37 @@ package lab1;
 class FizzBuzz {
 
     public static void main(String[] args) {
-
-        for (int i = 1; i <= 100; i++) {
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+        int num = 1;
+        while (num <= 100) {
+            num = doFizzBuzz(num);
         }
+    }
+
+    private static int doFizzBuzz(int num) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = num % 3 == 0;
+        boolean divisibleBy5 = num % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(num);
+
+        }
+
+        num += 1;
+        return num;
     }
 }
